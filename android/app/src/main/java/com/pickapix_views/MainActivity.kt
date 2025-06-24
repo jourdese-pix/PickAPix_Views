4b2e2c712c14d1108e8387805a6610b98b7c720d
Jourdese Palacio <jourdesepalacio@pixelmine.org>
2025-06-24 14:07:08 +0800

    Initial commit
    Generated by react-native@0.80.0

---
+++ android/app/src/main/java/com/pickapix_views/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.pickapix_views
+
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
+import com.facebook.react.defaults.DefaultReactActivityDelegate
+
+class MainActivity : ReactActivity() {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  override fun getMainComponentName(): String = "PickAPix_Views"
+
+  /**
+   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+   */
+  override fun createReactActivityDelegate(): ReactActivityDelegate =
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+}
